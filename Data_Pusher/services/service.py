efa0a294b3262b1a746155e59e57b6cc2b3452fb
devangparekh1 <devang.parekh@marutitech.com>
2022-08-10 14:24:25 +0530

    Add Service for the data pushing with external api call

--- Data_Pusher/services/service.py
+++ Data_Pusher/services/service.py
@@ -1,22 +1,27 @@
 import redis
 import random
+import requests
+from flask import jsonify
 
 r = redis.Redis(host="localhost", port=6379, db=0)
 
 
 class PushService:
     def data_push(self, data):
-        r.set(data["user_id"], 1) if not r.get(data["user_id"]) else r.incr(
-            data["user_id"]
-        )
-        cache = r.get(data["user_id"])  # get user_id from redis
-        random_number = random.randint(1, 10000)
+        try:
+            r.set(data["user_id"], 1) if not r.get(data["user_id"]) else r.incr(
+                data["user_id"]
+            )
 
-        payload = {
-            "user_id": data["user_id"],
-            "message": data["message"],
-            "cache": int(cache),
-            "random_number": random_number,
-        }
+            payload = {
+                "user_id": data["user_id"],
+                "message": data["message"],
+                "counter": int(r.get(data["user_id"])),
+                "random_number": random.randint(1, 100),
+            }
+            requests.post("http://127.0.0.1:3000/api/validate", json=payload)
 
-        
+            pass
+
+        except Exception as e:
+            print(e)
