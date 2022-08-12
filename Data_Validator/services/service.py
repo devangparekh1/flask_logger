9e9922a4a0eaeb39c32b4824965c4e1685bd9d58
devangparekh1 <devang.parekh@marutitech.com>
2022-08-12 17:25:50 +0530

    Revise data validator service

--- Data_Validator/services/service.py
+++ Data_Validator/services/service.py
@@ -3,6 +3,7 @@ import requests
 from flask import jsonify
 from datetime import date
 import time
+import asyncio
 
 # {
 #     "message": {
@@ -24,7 +25,7 @@ class ValidateService:
                 data["random_number"] = random.randint(1, 100)
                 data["count"] = 1
 
-                send_data(data)
+                asyncio.run(send_data(data))
                 time.sleep(4)  # Halt the execution for the 4 seconds
                 self.push_to_es(data)
 
@@ -34,21 +35,23 @@ class ValidateService:
             else:
                 data["category"] = "Direct"
 
-            return send_data(data)
+            return asyncio.run(send_data(data))
         except Exception as e:
-            print(e)
             return False
 
 
-def send_data(data_to_send):
+async def send_data(data_to_send):
     today = date.today()
     try:
         del data_to_send["count"]
         del data_to_send["random_number"]
         data_to_send["created_at"] = today.strftime("%Y-%m-%d")
         new_data_to_send = {"message": data_to_send}
-        requests.post("http://127.0.0.1:4000/api/messages", json=new_data_to_send)
+        asyncio.create_task(send_data_es(new_data_to_send))
         return True
     except Exception as e:
-        print(e)
         return False
+
+
+async def send_data_es(data_to_send):
+    requests.post("http://127.0.0.1:4000/api/messages", json=data_to_send)
